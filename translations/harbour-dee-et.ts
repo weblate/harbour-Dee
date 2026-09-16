2e9a922fc0b1f374282aed6c345917671f130028
Priit Jõerüüt <jrthwlate@users.noreply.hosted.weblate.org>
2026-09-16 20:17:15 +0200

    Translated using Weblate (Estonian)
    Currently translated at 100.0% (131 of 131 strings)
    
    Translation: Harbour Dee/Translations
    Translate-URL: https://hosted.weblate.org/projects/harbour-dee/translations/et/

--- translations/harbour-dee-et.ts
+++ translations/harbour-dee-et.ts
@@ -189,7 +189,7 @@
     </message>
     <message>
         <source>Sent private message</source>
-        <translation type="unfinished"></translation>
+        <translation>Saada privaatne sõnum</translation>
     </message>
 </context>
 <context>
@@ -373,31 +373,31 @@
     <name>PrivateMessagePage</name>
     <message>
         <source>Sent private message</source>
-        <translation type="unfinished"></translation>
+        <translation>Privaatne sõnum on saadetud</translation>
     </message>
     <message>
         <source>Private message</source>
-        <translation type="unfinished">Privaatne sõnum</translation>
+        <translation>Privaatne sõnum</translation>
     </message>
     <message>
         <source>To %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Adressaat: %1</translation>
     </message>
     <message>
         <source>From %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Saatja: %1</translation>
     </message>
     <message>
         <source>Write a reply…</source>
-        <translation type="unfinished"></translation>
+        <translation>Koosta vastus…</translation>
     </message>
     <message>
         <source>Sending…</source>
-        <translation type="unfinished"></translation>
+        <translation>Saadan…</translation>
     </message>
     <message>
         <source>Send</source>
-        <translation type="unfinished"></translation>
+        <translation>Saada</translation>
     </message>
 </context>
 <context>
